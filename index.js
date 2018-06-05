dfcecfbcc01c4498022dc67909c53df62ee9f843
Ahsan Naveed <anaveed@tesla.com>
2018-06-05 13:37:13 -0700

    Create in memory db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql'
+import { randomBytes } from 'crypto';
 
 import schema from './schema';
 
@@ -9,17 +10,47 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing!')
 })
 
-// create resolver for schema
-const root = {friend: () => {
-    return {
-        id: "2309799",
-        firstName: "Ahsan",
-        lastName: "Naveed",
-        email: "anaveed at sfu",
-        language: "en",
-        gender: "M"
+// in mem db
+class Friend {
+    constructor(id, {firstName, lastName, gender, language, email}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+        this.email = email;
+        this.gender = this.gender;
+        
     }
-}};
+}
+
+const friendDatabase = {}
+
+// create resolver for schema - would fetch the actual data from DB 
+const root = {
+    friend: () => {
+        return {
+            id: "2309799",
+            firstName: "Ahsan",
+            lastName: "Naveed",
+            emails: [
+                {email: "anaveed at sfu"},
+                {email: "anaveed at ubc"},
+                {email: "anaveed at mit"}
+            ],
+            language: "en",
+            gender: "M",
+            email: "anaveed at ubc"
+        }
+    },
+    friends: () => {
+        return Object.values(friendDatabase);
+    },
+    createFriend: ({input}) => {
+        let id = randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
+    }
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
--- schema.js
+++ schema.js
@@ -1,16 +1,36 @@
 import { buildSchema } from 'graphql';
 
+// building schema
 const schema = buildSchema(`
+
     type Friend {
-        id: ID! @unique
-        firstName: String
+        id: ID @unique
+        firstName: String!
         lastName: String
         gender: String
         language: String
         email: String
     }
-    type Query {
+
+    type Friends {
         friend: Friend
+    }
+    
+    input FriendInput {
+        id: ID @unique
+        firstName: String!
+        lastName: String
+        gender: String
+        language: String
+        email: String
+    }
+
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+    }
+
+    type Query {
+        friends: [Friend!]!
     }
 `)
 
